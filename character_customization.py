0dd8c92e162d01873a7ac6ac8985ab807c3ae326
Sebasto1474 <sebastiang.ledesma@gmail.com>
2025-12-22 22:13:04 +0000

    actualizando customization

--- character_customization.py
+++ character_customization.py
@@ -1,3 +1,5 @@
 def customization(genre, race, class_info):
     genre_options = ["Male", "Female"]
     race_options = ["Human", "Orc", "Elf", "Dark elf", "Dwarf", "Gnome"]
+    class_options = ["Warrior", "Paladin", "Mage", "Druid", "Priest", "Rogue"]
+    
